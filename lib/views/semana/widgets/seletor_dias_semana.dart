5052890dbe027303f886397a5f9cd21fef8cf414
Joao Douglas <josantos.inv@gmail.com>
2026-05-10 14:50:35 -0300

    feat: Adiçao de regra visual do dia da semana

--- lib/views/semana/widgets/seletor_dias_semana.dart
+++ lib/views/semana/widgets/seletor_dias_semana.dart
@@ -6,8 +6,43 @@ import '../../../core/theme/cores_app.dart';
 import '../../../core/utils/formatador_data.dart';
 import '../../../viewmodels/semana_viewmodel.dart';
 
-class SeletorDiasSemana extends StatelessWidget {
+class SeletorDiasSemana extends StatefulWidget {
   const SeletorDiasSemana({super.key});
+
+  @override
+  State<SeletorDiasSemana> createState() => _SeletorDiasSemanaState();
+}
+
+class _SeletorDiasSemanaState extends State<SeletorDiasSemana> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _ajustarScrollInicial();
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _ajustarScrollInicial() {
+    final hoje = DateTime.now();
+    // weekday % 7 → 0=Dom, 1=Seg, 2=Ter, 3=Qua, 4=Qui, 5=Sex, 6=Sáb
+    final posicaoNaSemana = hoje.weekday % 7;
+    if (posicaoNaSemana > 3) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    }
+  }
+
+  List<DateTime> _obterDiasDaSemana(DateTime referencia) {
+    final domingo = referencia.subtract(Duration(days: referencia.weekday % 7));
+    return List.generate(7, (i) => domingo.add(Duration(days: i)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +55,7 @@ class SeletorDiasSemana extends StatelessWidget {
     return SizedBox(
       height: 72,
       child: ListView.separated(
+        controller: _scrollController,
         scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.symmetric(horizontal: 20),
         itemCount: diasDaSemana.length,
@@ -38,11 +74,6 @@ class SeletorDiasSemana extends StatelessWidget {
         },
       ),
     );
-  }
-
-  List<DateTime> _obterDiasDaSemana(DateTime referencia) {
-    final domingo = referencia.subtract(Duration(days: referencia.weekday % 7));
-    return List.generate(7, (i) => domingo.add(Duration(days: i)));
   }
 }
 
